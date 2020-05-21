c4018294a7b9cc08bb58fb114764b262e3a03788
zhouyue <1440298847@qq.com>
2020-05-22 03:33:51 +0800

    第一次提交

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -14,31 +14,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
 
-
-        List<String> list = new ArrayList<>();
-        list.add("111");
-        list.add("222");
-        list.add("333");
-
-        for (String s : list) {
-            System.out.println(s);
-        }
-        String[] arr = new String[]{"aaa","bbb","ccc"};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-
-        }
-        Scanner sc = new Scanner(System.in);
-
-        int aaa=sc.nextInt();
-        System.out.println("aaa---"+aaa);
-
-        System.out.println("aaa");
-
-        int a = 1/0;
-
-        System.out.println("a--" + a);
-
     }
 
 }

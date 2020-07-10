4585e4900427b1ef268b38698f1d7807f7dca18a
zhouyue <1440298847@qq.com>
2020-07-11 03:37:22 +0800

    第二次提交

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -2,9 +2,7 @@ package com.example;
 
 import sun.security.provider.PolicySpiFile;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author ZY
@@ -13,6 +11,35 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
+//      System.out.println(System.currentTimeMillis());
+
+//      System.out.println(System.currentTimeMillis());
+
+        System.out.println(new Date(System.currentTimeMillis()));
+
+        System.out.println(new Date(1590114562848L));
+
+        /*****************test****************/
+       /*System.out.println("*****************test****************");
+
+        System.out.println("bool--"+"abc".contains(""));
+
+        System.out.println("one more line!!!");*/
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+//        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+//        cal.setTime(new Date());
+        cal.add(Calendar.HOUR_OF_DAY, -24);
+
+//        cal.add(Calendar.HOUR_OF_DAY, 24);
+
+        System.out.println("*************************");
+
+        System.out.println(cal);
+
+        System.out.println(cal.getTime());
 
     }
 

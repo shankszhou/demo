34cebc1ef03ce1f3f8318e435c4020b59fb4190e
zhouyue <1440298847@qq.com>
2020-07-11 03:44:26 +0800

    Revert "第二次提交"
    This reverts commit 4585e490

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -2,7 +2,9 @@ package com.example;
 
 import sun.security.provider.PolicySpiFile;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author ZY
@@ -11,35 +13,6 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
-//      System.out.println(System.currentTimeMillis());
-
-//      System.out.println(System.currentTimeMillis());
-
-        System.out.println(new Date(System.currentTimeMillis()));
-
-        System.out.println(new Date(1590114562848L));
-
-        /*****************test****************/
-       /*System.out.println("*****************test****************");
-
-        System.out.println("bool--"+"abc".contains(""));
-
-        System.out.println("one more line!!!");*/
-
-        Calendar cal = Calendar.getInstance();
-
-        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
-//        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-//        cal.setTime(new Date());
-        cal.add(Calendar.HOUR_OF_DAY, -24);
-
-//        cal.add(Calendar.HOUR_OF_DAY, 24);
-
-        System.out.println("*************************");
-
-        System.out.println(cal);
-
-        System.out.println(cal.getTime());
 
         System.out.println("*************************************");
 
